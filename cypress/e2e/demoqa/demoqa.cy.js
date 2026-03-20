a8a68409661ccc3ee3944e2ad82ab89a4c5a716b
ZeroConditio <65846036+ZeroConditio@users.noreply.github.com>
2026-03-20 18:18:33 +0200

    init

---
+++ cypress/e2e/demoqa/demoqa.cy.js
@@ -0,0 +1,13 @@
+import { describe } from "mocha";
+
+
+describe('DEMO QA', () => {
+    context('Selectable tests', () => {
+        beforeEach(() => {
+            cy.visit('XXXX');
+        });
+
+
+
+    });
+});
